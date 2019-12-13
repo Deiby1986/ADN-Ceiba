2f1c026c40b1b9b1f6536ccae25013de9b810d1c
Deiby Manzo <deiby1986@gmail.com>
2019-12-13 08:27:25 -0500

    Correccion de prueba de integracion para producto

--- src/test/java/com/co/ceiba/adn/infraestructura/TestProductCommand.java
+++ src/test/java/com/co/ceiba/adn/infraestructura/TestProductCommand.java
@@ -4,42 +4,51 @@ import static org.junit.Assert.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-import com.co.ceiba.adn.application.command.ProductCommand;
+import com.co.ceiba.adn.App;
 import com.co.ceiba.adn.application.command.bean.CommandProduct;
 import com.co.ceiba.adn.infraestructura.builder.CommandProductDataBuilder;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(ProductCommand.class)
-@ComponentScan("com")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+classes = App.class)
+@AutoConfigureMockMvc
 public class TestProductCommand {
 	
 	@Autowired
 	private ObjectMapper objetcMapper;
 	
 	@Autowired
-	private MockMvc mock; 
+	private WebApplicationContext wac;
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Before
+	public void setUp(){
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+	}
 
 	@Test
-	public void crear(){
-		/*CommandProduct prCommand= new CommandProductDataBuilder().build();
+	public void crearProducto() throws JsonProcessingException, Exception{
+		CommandProduct prCommand= new CommandProductDataBuilder().build();		
 		
-		
-			mock.perform(get("/api/products").
-							contentType(MediaType.APPLICATION_JSON_UTF8).
-							content(objetcMapper.writeValueAsString(prCommand))).andExpect(status().isOk());*/
-		
-		assertTrue(true);
+		mockMvc.perform(get("/api/products").
+							contentType(MediaType.APPLICATION_JSON).
+							content(objetcMapper.writeValueAsString(prCommand))).andExpect(status().isOk());
 		
 		
 	}
