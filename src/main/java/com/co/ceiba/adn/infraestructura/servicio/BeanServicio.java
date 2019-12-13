cac4b0c0d2979e4297bd2900be296fe8953c6fd8
Deiby Manzo <deiby1986@gmail.com>
2019-12-13 08:56:08 -0500

    modificacion de bean de servicio

--- src/main/java/com/co/ceiba/adn/infraestructura/servicio/BeanServicio.java
+++ src/main/java/com/co/ceiba/adn/infraestructura/servicio/BeanServicio.java
@@ -1,12 +1,14 @@
 package com.co.ceiba.adn.infraestructura.servicio;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.co.ceiba.adn.domain.services.ProductService;
+import com.co.ceiba.adn.domain.services.SalesDetailService;
 import com.co.ceiba.adn.infraestructura.dao.ProductDao;
+import com.co.ceiba.adn.infraestructura.dao.SalesDetailDao;
 import com.co.ceiba.adn.infraestructura.repository.ProductRepositoryDB;
+import com.co.ceiba.adn.infraestructura.repository.SalesDetailRepositoryDB;
 
 
 
@@ -17,5 +19,10 @@ public class BeanServicio {
 	public ProductService createProductService(ProductDao daopDao) {		
 		return new ProductService(new ProductRepositoryDB(daopDao));
 	} 
+	
+	@Bean
+	public SalesDetailService createDetailService(SalesDetailDao daoDetail) {
+		return new SalesDetailService(new SalesDetailRepositoryDB(daoDetail));
+	}
 
 }
