c16f138c6cfe6fa83c0db38c112995d618a9ed04
Deiby Manzo <deiby1986@gmail.com>
2019-12-17 16:28:44 -0500

    Correccion de Smells

--- backend/src/main/java/com/co/ceiba/adn/domain/services/SalesHeaderService.java
+++ backend/src/main/java/com/co/ceiba/adn/domain/services/SalesHeaderService.java
@@ -5,8 +5,6 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.regex.Pattern;
 
-import javax.swing.text.DateFormatter;
-
 import com.co.ceiba.adn.domain.exceptions.CanSellOnWekendsException;
 import com.co.ceiba.adn.domain.exceptions.InvalidDateException;
 import com.co.ceiba.adn.domain.model.entities.SalesHeader;
@@ -22,7 +20,7 @@ public class SalesHeaderService {
 	}
 	
 	public SalesHeader execute(SalesHeader header) {
-		if(!DateFormatCorrect(header.getDate()))
+		if(!dateFormatCorrect(header.getDate()))
 			throw new InvalidDateException("Especifique la fecha en formato dd/mm/yyyy");
 		if(!canSale(header))
 			throw new CanSellOnWekendsException("No se puede vender los fines de semana");
@@ -33,12 +31,11 @@ public class SalesHeaderService {
 		String stringDate = header.getDate();
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		LocalDate date = LocalDate.parse(stringDate, formatter);
-		if(date.getDayOfWeek() ==DayOfWeek.SATURDAY || date.getDayOfWeek()== DayOfWeek.SUNDAY)
-			return false;
-		return true;		
+		return !(date.getDayOfWeek() ==DayOfWeek.SATURDAY || date.getDayOfWeek()== DayOfWeek.SUNDAY);
+					
 	}
 	
-	public boolean DateFormatCorrect(String date) {
+	public boolean dateFormatCorrect(String date) {
 		String regexp = "\\d{1,2}/\\d{1,2}/\\d{4}";
 		return Pattern.matches(regexp, date);		
 	}
