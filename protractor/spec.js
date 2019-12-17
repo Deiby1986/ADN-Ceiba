dcaca4b5befd5d1f48a3bfe103de814f635f908c
Deiby Manzo <deiby1986@gmail.com>
2019-12-17 14:23:48 -0500

    Cambios del nombre de los elementos del frontend

---
+++ protractor/spec.js
@@ -0,0 +1,23 @@
+describe('Salesfront test', function() {
+  it('Should load', function() {
+    browser.get('http://localhost:4200/');
+
+    expect(browser.getTitle()).toEqual('Salesfront');
+  });
+  
+  it('list products', function() {
+    browser.get('http://localhost:4200/products');
+    expect(element(by.tagName('table')).isDisplayed()).toBeTruthy();    
+  });
+  
+  it('save product', function() {
+    browser.get('http://localhost:4200/product');
+	var codigo = parseInt(Math.random()*100);
+	element(by.name('codigo')).sendKeys("PROTRACTOR"+codigo);
+	element(by.name('nombre')).sendKeys("Prueba de protractor");
+	element(by.name('precio')).sendKeys("10.0");
+	element(by.name('cantidad')).sendKeys("10");
+	element(by.name('guardar')).click();
+    expect(element(by.tagName('table')).isDisplayed()).toBeTruthy();    
+  });
+});
