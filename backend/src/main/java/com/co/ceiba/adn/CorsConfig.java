e83da4aa38d33d372239556b0bdfecb8ba6cac22
Deiby Manzo <deiby1986@gmail.com>
2019-12-26 11:16:14 -0500

    Se quito Configuracion CORS del backend ya que se maneja con un proxy en el front

--- backend/src/main/java/com/co/ceiba/adn/CorsConfig.java
+++ backend/src/main/java/com/co/ceiba/adn/CorsConfig.java
@@ -1,17 +1,15 @@
 package com.co.ceiba.adn;
 
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 
-@Configuration
+//@Configuration
 public class CorsConfig {
 	
-	 @Bean
+	 //@Bean
 	    public CorsFilter corsFilter() {
 	        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 	        CorsConfiguration config = new CorsConfiguration();
