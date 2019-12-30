d1d344d58764e6bb9960d22273c6e9d5cdc92435
Deiby Manzo <deiby1986@gmail.com>
2019-12-30 14:25:10 -0500

    Prueba para detalles del producto

--- backend/src/test/java/com/co/ceiba/adn/domain/SalesDetailTest.java
+++ backend/src/test/java/com/co/ceiba/adn/domain/SalesDetailTest.java
@@ -6,8 +6,11 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.Test;
 
+import com.co.ceiba.adn.domain.builder.ProductTestDataBuilder;
 import com.co.ceiba.adn.domain.builder.SalesDetailTestDataBuilder;
 import com.co.ceiba.adn.domain.exceptions.FieldEmptyOrNullException;
+import com.co.ceiba.adn.domain.exceptions.NoStockAvailableForProduct;
+import com.co.ceiba.adn.domain.model.entities.Product;
 
 public class SalesDetailTest {
 
@@ -27,9 +30,23 @@ public class SalesDetailTest {
 	
 	@Test
 	public void testSalesDetailQtyZero() {
+		SalesDetailTestDataBuilder builder = new SalesDetailTestDataBuilder();		
+		assertThrows(FieldEmptyOrNullException.class, ()->builder.withQtyPurchased(0L).build());		
+	}	
+	
+	@Test
+	public void testTotalLessThanZero() {
+		SalesDetailTestDataBuilder builder = new SalesDetailTestDataBuilder();		
+		assertThrows(FieldEmptyOrNullException.class, ()->builder.withTotal(0L).build());
+	}
+	
+	@Test
+	public void testNoStock() {
+		ProductTestDataBuilder productBuilder = new ProductTestDataBuilder();
+		Product product = productBuilder.withQty(100L).build();		
 		SalesDetailTestDataBuilder builder = new SalesDetailTestDataBuilder();
 		
-		assertThrows(FieldEmptyOrNullException.class, ()->builder.withQtyPurchased(0L).build());		
+		assertThrows(NoStockAvailableForProduct.class, ()->builder.withProduct(product).withQtyPurchased(200L).build());
 	}	
 
 }
